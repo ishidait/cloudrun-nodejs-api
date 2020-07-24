237c90d74f8cb6a2bfabb0b0006618f155ee34f2
Makoto Ishida <makotoishida@gmail.com>
2020-07-23 19:24:01 -1000

    Add Google Translate API.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const { Translate } = require('@google-cloud/translate').v2;
+const client = new Translate();
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`API listening on port ${port}.`);
@@ -10,4 +13,9 @@ app.get('/hello', async (req, res) => {
   const name = req.query.name;
   const result = `Hello ${name}!\n\n`;
   res.send(result);
+});
+
+app.get('/translate/:lang', async (req, res) => {
+  const result = await client.translate('Hello, world!', req.params.lang);
+  res.send(`${result[0]}\n\n`);
 });
---
+++ start.sh
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/mike/Development/Web/cloud-run-nodejs-api-a801f458db44.json
+
+npm start
